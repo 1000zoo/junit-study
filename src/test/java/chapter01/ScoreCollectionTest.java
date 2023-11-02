4904df67c8da54acda253d17a97c0ff53d59fdcc
1000zoo <cjswldn99@gmail.com>
2023-11-03 00:24:02 +0900

    chapter01 - 테스트 준비, 실행, 단언

---
+++ src/test/java/chapter01/ScoreCollectionTest.java
@@ -0,0 +1,22 @@
+package chapter01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ScoreCollectionTest {
+
+    @Test
+    public void answerArithmeticMeanOfTwoNumbers() {
+        // given
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // when
+        int actualResult = collection.arithmeticMean();
+
+        // then
+        assertThat(actualResult).isEqualTo(6);
+    }
+}
